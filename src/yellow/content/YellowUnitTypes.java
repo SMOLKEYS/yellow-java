e5aa0b30ac27339e13c5c431dcc02ed76035eef1
SMOLKEYS <jiroshimonis@gmail.com>
2021-11-04 15:06:46 +0800

    graphiks

--- src/yellow/content/YellowUnitTypes.java
+++ src/yellow/content/YellowUnitTypes.java
@@ -61,11 +61,15 @@ public class YellowUnitTypes implements ContentList{
                 }}
                 /*new Weapon("bullethell"){{}}*/
             );
-            /*
+            //Code by Emanuel G.
             draw(){
-                hhh what do i use here
+                int circles = 8; 
+                Draw.color(Tmp.c1.set(Color.yellow).lerp(Color.white,Mathf.absin(Time.time, 10,1)));
+                for(let i = 0; i < circles; i++){Draw.z(Layer.effect + 0.001);
+                let x = Angles.trnsx(Time.time + i * (360 / circles), 3), y = Angles.trnsy(Time.time + i * (360 / circles), 15);
+                Draw.rect(Core.atlas.find("alpha"), this.x + x * Math.sin(Time.time * 0.001) * y * x / y * x, this.y + y * Math.sin(Time.time * 0.001) * y * x / y * x, x, y, Mathf.angle(x, y))
+                }
             }
-            */
         }};
         
         yellowGround = new UnitType("yellowGround"){{
