1cc8b31e978916c38f868ec16f7b95a09def2a7b
SMOLKEYS <jiroshimonis@gmail.com>
2021-11-18 13:40:23 +0800

    technician

--- src/yellow/content/YellowUnitTypes.java
+++ src/yellow/content/YellowUnitTypes.java
@@ -53,7 +53,7 @@ public class YellowUnitTypes implements ContentList{
             defaultController = DefenderAI::new;
             region = Core.atlas.find("yellow");
             
-            weapons.add(meltdownShotgun.copy());
+            weapons.add(YellowWeapons.meltdownShotgun.copy());
         }
             @Override
             public void setStats(){
