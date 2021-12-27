7108720aaa43eb747bd35e593a42c6429823fbb5
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-12-27 22:48:08 +0300

    add button
    WeaponSwitch is a Fragment now

--- src/yellow/ui/buttons/YellowWeaponSwitch.java
+++ src/yellow/ui/buttons/YellowWeaponSwitch.java
@@ -1,9 +1,28 @@
 package yellow.ui.buttons;
 
+import arc.scene.*;
+import arc.scene.ui.layout.*;
+import arc.scene.style.*;
 import yellow.ui.buttons.dialogs.*;
+import mindustry.ui.fragments.*;
 
-public class YellowWeaponSwitch{
+import static arc.Core.*;
+
+public class YellowWeaponSwitch extends Fragment{
+
+    private static final float minimapWidth = 150f;
 
     private YellowWeaponSwitchDialog dialog = new YellowWeaponSwitchDialog();
+
+    public void build(Group parent){
+        parent.fill(cont -> {
+            cont.name = "weapons switch";
+            cont.defaults().size(minimapWidth / 2f);
+            cont.marginRight(Scl.scl(minimapWidth));
+
+            Drawable icon = atlas.drawable("status-disarmed");
+            cont.button(icon, dialog::show);
+        });
+    }
 
 }
