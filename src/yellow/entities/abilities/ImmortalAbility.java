0cf1ccaeaccb584cab2ad05d0111e58becf6c28b
SMOLKEYS <jiroshimonis@gmail.com>
2021-12-27 21:30:17 +0800

    whahuh????

--- src/yellow/entities/abilities/ImmortalAbility.java
+++ src/yellow/entities/abilities/ImmortalAbility.java
@@ -7,6 +7,7 @@ import arc.util.*;
 import mindustry.*;
 import mindustry.content.*;
 import mindustry.entities.*;
+import mindustry.entities.abilities.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
