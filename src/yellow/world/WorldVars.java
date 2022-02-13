6722ce4e3569bbe431c75a2460fe798c8b054275
SMOLKEYS <jiroshimonis@gmail.com>
2022-02-13 23:20:20 +0800

    hmmmmmmmmmmmmmm

--- src/yellow/world/WorldVars.java
+++ src/yellow/world/WorldVars.java
@@ -16,13 +16,21 @@ public class WorldVars{
     
     //Menu time. Resets whenever you leave the game or enter a world.
     //Unlocks an achivement once 1 hour has passed.
-    public static int currentSessionMenuTime = settings.getInt("current-session-menu-time");
+    public static int currentSessionMenuTime(){
+        return settings.getInt("current-session-menu-time");
+    }
     //Menu time from all sessions.
-    public static int allSessionsMenuTimes = settings.getInt("all-sessions-menu-times");
+    public static int allSessionsMenuTimes(){
+        return settings.getInt("all-sessions-menu-times");
+    }
     //Kill count.
-    public static int killCount = settings.getInt("kill-count");
+    public static int killCount(){
+        return settings.getInt("kill-count");
+    }
     //........ehehehe.
-    public static boolean horny = settings.getBool("horny");
+    public static boolean horny(){
+        return settings.getBool("horny");
+    }
     
     //Prepares all variables *once*.
     public static void prepare(){
@@ -40,15 +48,15 @@ public class WorldVars{
     public static void start(){
         Timer.schedule(() -> {
             if(!state.isGame()){
-                settings.put("current-session-menu-time", this.currentSessionMenuTime + 1);
-                settings.put("all-sessions-menu-times", this.allSessionsMenuTimes + 1);
+                settings.put("current-session-menu-time", currentSessionMenuTime + 1);
+                settings.put("all-sessions-menu-times", allSessionsMenuTimes + 1);
             } else {
                 settings.put("current-session-menu-time", 0);
             };
             
         }, 1f, 1f, -1f);
         
-        Events.run(UnitDestroyEvent.class, () -> settings.put("kill-count", this.killCount + 1));
-        Events.run(UnitDrownEvent.class, () -> settings.put("kill-count", this.killCount - 1)); //thats their skill issue, not yours
+        Events.run(UnitDestroyEvent.class, () -> settings.put("kill-count", killCount + 1));
+        Events.run(UnitDrownEvent.class, () -> settings.put("kill-count", killCount - 1)); //thats their skill issue, not yours
     }
 }
