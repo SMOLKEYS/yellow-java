55c0c92a7f31ae2eacc94fd0b30ba66fdb1207ec
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-04-06 01:03:59 +0800

    a

--- src/yellow/interactions/ui/DialogueBox.java
+++ src/yellow/interactions/ui/DialogueBox.java
@@ -11,11 +11,11 @@ public class DialogueBox{
     private static final float width = 250f, height = 270f;
     
     public void build(){
-        ui.hudGroup.find("minimap/position").row();
-        ui.hudGroup.find("minimap/position").children.get(0).actions(Actions.moveBy(80f, 0f));
-        ui.hudGroup.find("minimap/position").children.get(1).actions(Actions.moveBy(80f, 0f));
+        ui.hudGroup.find("minimap/position").class.row();
+        ui.hudGroup.find("minimap/position").class.children.get(0).actions(Actions.moveBy(80f, 0f));
+        ui.hudGroup.find("minimap/position").class.children.get(1).actions(Actions.moveBy(80f, 0f));
         
-        ui.hudGroup.find("minimap/position").table(t -> {
+        ui.hudGroup.find("minimap/position").class.table(t -> {
             t.name = "dialogue box";
             t.size(width, height);
             
