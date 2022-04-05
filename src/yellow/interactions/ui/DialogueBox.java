bd69385864de24200999f04702ae80db40a4b81d
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-04-06 00:55:36 +0800

    ve

--- src/yellow/interactions/ui/DialogueBox.java
+++ src/yellow/interactions/ui/DialogueBox.java
@@ -2,6 +2,7 @@ package yellow.interactions.ui;
 
 import mindustry.ui.fragments.Fragment;
 import arc.scene.*;
+import arc.scene.ui.layout.*;
 import arc.scene.actions.*;
 import arc.flabel.*;
 
@@ -9,11 +10,11 @@ public class DialogueBox extends Fragment{
     private static final float width = 250f, height = 270f;
     
     public void build(Group parent){
-        parent.row();
-        parent.children.get(0).actions(Actions.moveBy(80f, 0f));
-        parent.children.get(1).actions(Actions.moveBy(80f, 0f));
+        parent.find("minimap/position").row();
+        parent.find("minimap/position").children.get(0).actions(Actions.moveBy(80f, 0f));
+        parent.find("minimap/position").children.get(1).actions(Actions.moveBy(80f, 0f));
         
-        parent.table(t -> {
+        parent.find("minimap/position").table(t -> {
             t.name = "dialogue box";
             t.size(width, height);
             
