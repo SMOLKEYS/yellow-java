d548085917ef171eda57da96543edbe68f696b18
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-04-06 08:33:54 +0800

    test

--- src/yellow/interactions/ui/DialogueBox.java
+++ src/yellow/interactions/ui/DialogueBox.java
@@ -4,6 +4,7 @@ import arc.scene.*;
 import arc.scene.ui.layout.*;
 import arc.scene.actions.*;
 import arc.flabel.*;
+import mindustry.ui.*;
 
 import static mindustry.Vars.*;
 
@@ -12,18 +13,15 @@ public class DialogueBox{
     
     public void build(){
         ((Table) ui.hudGroup.find("minimap/position")).row();
-        ((Table) ui.hudGroup.find("minimap/position")).children.get(0).actions(Actions.moveBy(80f, 0f));
-        ((Table) ui.hudGroup.find("minimap/position")).children.get(1).actions(Actions.moveBy(80f, 0f));
+        ((Table) ui.hudGroup.find("minimap/position")).getChildren().get(0).actions(Actions.moveBy(80f, 0f));
+        ((Table) ui.hudGroup.find("minimap/position")).getChildren().get(1).actions(Actions.moveBy(80f, 0f));
         
         ((Table) ui.hudGroup.find("minimap/position")).table(t -> {
             t.name = "dialogue box";
-            t.size(width, height);
+            t.setSize(width, height);
+            t.background(Styles.black6)
             
-            t.label(l -> {
-                l.name = "dialogue text";
-                l.setWrap(true);
-                l.setText(new FLabel("{wave}..."));
-            });
+            t.add(new FLabel("{wave}....."));
         });
     }
 }
