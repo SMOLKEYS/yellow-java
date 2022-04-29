4ff17b917a27765590239db5c14aa52f77391b31
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-04-29 19:50:18 +0800

    jabibi among us

--- src/yellow/world/blocks/units/SummoningShrine.java
+++ src/yellow/world/blocks/units/SummoningShrine.java
@@ -4,11 +4,14 @@ import arc.util.*;
 import arc.math.*;
 import arc.scene.ui.layout.*;
 import arc.flabel.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
 import mindustry.gen.*;
 import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.content.*;
 import mindustry.entities.*;
+import mindustry.graphics.*;
 
 import static mindustry.Vars.*;
 
@@ -32,18 +35,46 @@ public class SummoningShrine extends Block{
     
     public class SummoningShrineBuild extends Building{
         
+        private boolean currentlySummoning = false;
+        private float a = 0f;
+        
+        public boolean summoning(){
+            return currentlySummoning;
+        }
+        
         @Override
         public void buildConfiguration(Table table){
             table.table(t -> {
                 t.add("Summoning Shrine (" + unit.localizedName + ")").row();
                 t.button("Summon Unit", () -> { /** TODO button wrappinģ screwery*/
                     requestEffect.at(this);
+                    currentlySummoning = true;
                     Time.run(summonTime, () -> {
                         unit.spawn(team, this);
                         summonEffect.at(this);
+                        currentlySummoning = false;
                     });
                 });
             });
+        
+        
+        @Override
+        public void draw(){
+            super.draw();=
+            float lerpA = summoning() ? 1f : 0f;
+            float sus = Mathf.absin(10f, 10f);
+            a = Mathf.lerp(a, lerpA, 0.04f);
+            
+            Draw.z(Layer.effect);
+            Draw.color(Tmp.c1.set(Color.yellow).lerp(Color.cyan, Mathf.absin(10f, 1f)));
+            Fill.circle(this, 5f + Mathf.absin(10f, 2f));
+            Lines.circle(this, 20f);
+            Lines.square(this, 19f, Time.time);
+            Lines.square(this, 19f, -Time.time);
+            Draw.alpha(a);
+            Lines.circle(this, 25f + sus);
+            Lines.square(this, 25f + sus, Time.time);
+            Lines.square(this, 25f + sus, -Time.time);
         }
     }
 }
