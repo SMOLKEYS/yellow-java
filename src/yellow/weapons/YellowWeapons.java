7deeef69d91e69ab44ccf984c617388362a90bf5
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-05-16 00:53:40 +0800

    ok hotshit

--- src/yellow/weapons/YellowWeapons.java
+++ src/yellow/weapons/YellowWeapons.java
@@ -194,10 +194,10 @@ public class YellowWeapons{
         
         //region mirrored
         
-        meltdownBurstAttack2 = ((DisableableWeapon) meltdownBurstAttack.copy());
-        antiMothSpray2 = ((DisableableWeapon) antiMothSpray.copy());
-        decimation2 = ((DisableableWeapon) decimation.copy());
-        airstrikeFlareLauncher2 = ((DisableableWeapon) airstrikeFlareLauncher.copy());
+        meltdownBurstAttack2 = ((NameableWeapon) meltdownBurstAttack.copy());
+        antiMothSpray2 = ((NameableWeapon) antiMothSpray.copy());
+        decimation2 = ((NameableWeapon) decimation.copy());
+        airstrikeFlareLauncher2 = ((NameableWeapon) airstrikeFlareLauncher.copy());
         
         meltdownBurstAttack2.x = meltdownBurstAttack.x - (meltdownBurstAttack.x * 2);
         meltdownBurstAttack2.reload = meltdownBurstAttack.reload * 2;
