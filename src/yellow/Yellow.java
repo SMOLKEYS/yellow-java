7a0df61e9fe4d2143ec99555462127bbb075ed88
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-05-19 23:44:57 +0800

    192.168.27.239

--- src/yellow/Yellow.java
+++ src/yellow/Yellow.java
@@ -43,10 +43,10 @@ public class Yellow extends Mod{
                 DialogueBox.build();
             });
             Time.runTask(120f, () -> {
-                DialogueBox.dialogueStart(Responses.test, {
+                DialogueBox.dialogueStart(Responses.test, new Runnable[]{
                     () -> {ui.showInfo("amogus")},
                     () -> {ui.showInfo("sus")}
-                }, {2, 5});
+                }, new Float[]{2, 5});
             });
             
             app.post(() -> {
