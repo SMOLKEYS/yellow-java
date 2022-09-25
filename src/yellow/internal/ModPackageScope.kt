00af167bde4d0b28a794f36a63ba7cf920333bd7
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-09-25 16:09:10 +0800

    hurb

--- src/yellow/internal/ModPackageScope.kt
+++ src/yellow/internal/ModPackageScope.kt
@@ -2,20 +2,20 @@ package yellow.internal
 
 import arc.struct.*
 import rhino.*
+import mindustry.Vars
 import mindustry.game.EventType.*
 
 //sh1p you have done it
 open class ModPackageScope{
-    var p: NativeJavaPackage? = null
-    
+
     fun load(){
         var scope = Vars.mods.scripts.scope as ImporterTopLevel
         
         val packages = arrayOf("yellow.content", "yellow.weapons")
         
         packages.forEach{
-            p = NativeJavaPackage(it, Vars.mods.mainLoader())
-            p.setParentScope(scope)
+            var p = NativeJavaPackage(it, Vars.mods.mainLoader())
+            p.setParentScope(scope?)
             scope.importPackage(p)
         }
     }
