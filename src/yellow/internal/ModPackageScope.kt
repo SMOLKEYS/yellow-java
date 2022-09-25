37205312d21afe85026cdc40c2c51c3dcb5688d9
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-09-25 16:36:47 +0800

    all of them

--- src/yellow/internal/ModPackageScope.kt
+++ src/yellow/internal/ModPackageScope.kt
@@ -11,7 +11,26 @@ open class ModPackageScope{
     fun load(){
         var scope = Vars.mods.scripts.scope as ImporterTopLevel
         
-        val packages = arrayOf("yellow.content", "yellow.weapons")
+        val packages = arrayOf(
+            "yellow",
+            "yellow.content",
+            "yellow.weapons",
+            "yellow.ctype",
+            "yellow.internal",
+            "yellow.internal.util",
+            "yellow.type",
+            "yellow.game",
+            "yellow.world",
+            "yellow.world.blocks",
+            "yellow.world.blocks.units",
+            "yellow.ui",
+            "yellow.ui.buttons",
+            "yellow.ui.buttons.dialogs",
+            "yellow.entities",
+            "yellow.entities.bullet",
+            "yellow.entities.units",
+            "yellow.entities.units.entity",
+        )
         
         packages.forEach{
             var p = NativeJavaPackage(it, Vars.mods.mainLoader())
