c937966d34b242a23579a91ade1f9c73ee11789f
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-10-09 00:53:03 +0800

    even MORE death prevention

--- src/yellow/entities/units/entity/YellowUnitEntity.kt
+++ src/yellow/entities/units/entity/YellowUnitEntity.kt
@@ -1,28 +1,35 @@
 package yellow.entities.units.entity
 
 import arc.math.Mathf
+import arc.math.geom.Vec2
 import arc.util.io.Reads
 import arc.util.io.Writes
 import mindustry.Vars
 import mindustry.gen.EntityMapping
 import mindustry.gen.UnitEntity
+import mindustry.content.UnitTypes
 import mindustry.entities.Units
+import mindustry.entities.bullet.BulletType
 import yellow.entities.units.YellowUnitType
 import yellow.game.YellowPermVars
-
 
 open class YellowUnitEntity: UnitEntity() {
 
     private var inited = false
+    private var franticTeleportTime = 60f
 
     var lives = 0
     var firstDeath = false
     var allowsHealing = false
+    var panicMode = false
+    var panicModeTypeTwo = false
 
     fun initVars() {
         inited = true
         lives = type().maxLives
         allowsHealing = Mathf.chance(0.346)
+        panicMode = Mathf.chance(0.221)
+        panicModeTypeTwo = Mathf.chance(0.124)
     }
     
     fun outOfWorldBounds(): Boolean{
@@ -54,7 +61,6 @@ open class YellowUnitEntity: UnitEntity() {
             x = x + Mathf.range(25f * 8f)
             y = y + Mathf.range(25f * 8f)
         }
-        
     }
 
     fun forceKill() {
@@ -107,7 +113,7 @@ open class YellowUnitEntity: UnitEntity() {
         //heal surrounding units; normal units gain 10 health, player units gain either no health or a third of their current health
         if(allowsHealing){
             Units.nearby(x, y, 15f*8f, 15f*8f){a: mindustry.gen.Unit ->
-                if(a.team != team) return
+                if(a.team != team return
                 if(!a.isPlayer()){
                     if(Mathf.chanceDelta(0.09)){
                         a.heal(10f)
@@ -119,6 +125,34 @@ open class YellowUnitEntity: UnitEntity() {
                 }
             }
         }
+        
+        
+        //teleport everywhere and start firing all weapons on last life for a second if panic mode is enabled
+        if(panicMode && lives == 1 && !(franticTeleportTime <= 0f)){
+            val locor = Vec2(Mathf.random(Vars.world.width()) * 8f, Mathf.random(Vars.world.height()) * 8f)
+            x = locor.x
+            y = locor.y
+            mounts.forEach{
+                it.shoot = true
+                it.weapon.update(this, it)
+            }
+            
+            //if type two panic mode is enabled, start dropping quad bombs
+            if(panicModeTypeTwo){
+                BulletType.createBullet(
+                    UnitTypes.quad.weapons[0].bullet,
+                    team,
+                    x,
+                    y,
+                    0f,
+                    256f,
+                    0f,
+                    1f
+                )
+            }
+            
+            franticTeleportTime--
+        }
     }
 
 
@@ -126,7 +160,11 @@ open class YellowUnitEntity: UnitEntity() {
         super.write(write)
         write.bool(inited)
         write.bool(firstDeath)
+        write.bool(allowsHealing)
+        write.bool(panicMode)
+        write.bool(panicModeTypeTwo)
         write.i(lives)
+        write.f(franticTeleportTime)
 
         /*
         mounts.forEach{
@@ -140,7 +178,12 @@ open class YellowUnitEntity: UnitEntity() {
         super.read(read)
         inited = read.bool()
         firstDeath = read.bool()
+        allowsHealing = read.bool()
+        panicMode = read.bool()
+        panicModeTypeTwo = read.bool()
         lives = read.i()
+        franticTeleportTime = read.f()
+        
         /*
         mounts.forEach{
             val e = it as DisableableWeaponMount
