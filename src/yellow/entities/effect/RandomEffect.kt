903eaa810666e1c1ae4d49c0b7313aa4f2b7fa59
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-10-15 15:27:47 +0800

    d

---
+++ src/yellow/entities/effect/RandomEffect.kt
@@ -0,0 +1,11 @@
+package yellow.entities.effect
+
+import mindustry.entities.Effect
+
+open class RandomEffect(var effects: Array<Effect>) : Effect(0f, {}){
+    
+    override fun create(x: Float, y: Float, rotation: Float, color: Color, data: Any?){
+        if(!shouldCreate()) return
+        effects.random().create(x, y, rotation, color, data)
+    }
+}
