418e18bdceae6681d75051d8eda986c6e86d9439
smol <75618732+SMOLKEYS@users.noreply.github.com>
2022-10-16 17:38:56 +0800

    Update YellowClassGateway.kt

--- src/yellow/internal/YellowClassGateway.kt
+++ src/yellow/internal/YellowClassGateway.kt
@@ -16,10 +16,10 @@ open class YellowClassGateway{
         val packages = Vars.tree.get("classpaths/yellow-classpath.txt").readString().split('\n')
 
         Log.info("[yellow]--------STARTING GATEWAY--------[]")
-        Log.info("Scope: $scope, Generator: $this, Classpath Container: $packages")
+        Log.info("Scope: $scope, Generator: $this, Class Loader: ${Vars.mods.mainLoader()}")
         packages.forEach{
-            var p = NativeJavaPackage(it, Vars.mods.mainLoader())
-            Log.info("importing classes from $p...")
+            val p = NativeJavaPackage(it, Vars.mods.mainLoader())
+            Log.info("importing classes from $it...")
             p.parentScope = scope
             scope.importPackage(p)
         }
