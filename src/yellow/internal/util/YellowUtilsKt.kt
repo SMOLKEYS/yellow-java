79176d4ec331d9e978b32220b40d4d82afe0d24b
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-10-17 15:27:55 +0800

    idior

--- src/yellow/internal/util/YellowUtilsKt.kt
+++ src/yellow/internal/util/YellowUtilsKt.kt
@@ -1,13 +1,21 @@
 package yellow.internal.util
 
+import arc.util.Log
 import arc.files.Fi
 import arc.struct.Seq
 
 object YellowUtilsKt{
     fun traverse(dir: Fi, dump: Seq<String>){
         dir.seq().each{ su: Fi ->
-            dump.add(su.pathWithoutExtension().toString().replace("/", ".").removeSuffix("."))
+            if(su.isDirectory()) dump.add(su.pathWithoutExtension().toString().replace("/", ".").removeSuffix("."))
             if(!su.seq().isEmpty) traverse(su, dump)
+        }
+    }
+    
+    fun traverse(dir: Fi){
+        dir.seq().each{ su: Fi -> 
+            if(su.isDirectory()) Log.info(su.pathWithoutExtension().toString().replace("/", ".").removeSuffix("."))
+            if(!su.seq().isEmpty) traverse(su)
         }
     }
 }
