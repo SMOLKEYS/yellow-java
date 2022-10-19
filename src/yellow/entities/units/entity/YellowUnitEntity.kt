1269173f84424f4a53c08986cb71e78a3fc9a87d
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-10-19 20:12:35 +0800

    hurr derr pow

--- src/yellow/entities/units/entity/YellowUnitEntity.kt
+++ src/yellow/entities/units/entity/YellowUnitEntity.kt
@@ -4,18 +4,20 @@ import arc.math.Mathf
 import arc.math.geom.Vec2
 import arc.util.io.Reads
 import arc.util.io.Writes
+import arc.struct.Seq
 import mindustry.Vars
 import mindustry.content.UnitTypes
 import mindustry.entities.Units
-import mindustry.gen.EntityMapping
-import mindustry.gen.Icon
-import mindustry.gen.UnitEntity
+import mindustry.gen.*
 import yellow.entities.units.DisableableWeaponMount
 import yellow.entities.units.YellowUnitType
 import yellow.game.YellowPermVars
 
 @Suppress("MemberVisibilityCanBePrivate", "unused")
 open class YellowUnitEntity: UnitEntity(){
+
+    @JvmStatic
+    val entities = Seq<YellowUnitEntity>()
 
     private var inited = false
     private var franticTeleportTime = 60f
@@ -26,20 +28,30 @@ open class YellowUnitEntity: UnitEntity(){
     var allowsHealing = false
     var panicMode = false
     var panicModeTypeTwo = false
+    var existenceTime = 0f
+
     
-    private fun initVars() {
+    private fun initVars(){
         inited = true
         lives = type().maxLives
         allowsHealing = Mathf.chance(0.346)
         panicMode = Mathf.chance(0.221)
         panicModeTypeTwo = Mathf.chance(0.124)
+        entities.add(this)
     }
     
-    fun outOfWorldBounds(): Boolean{
-        return x > Vars.world.width() * 8f || x < 0f || y > Vars.world.height() * 8f || y < 0f
+    private fun invalidateVars(){
+        lives = 0
+        allowsHealing = false
+        panicMode = false
+        panicModeTypeTwo = false
+        dead = true
+        health = 0
+        shield = 0
+        entities.remove(this)
     }
-
-    fun invalidateDeath() {
+    
+    private fun invalidateDeath(){
         lives -= 1
         health = type.health
         dead = false
@@ -69,11 +81,45 @@ open class YellowUnitEntity: UnitEntity(){
             y += Mathf.range(25f * 8f)
         }
     }
+    
+    private fun lifetimeCompare(runnable: () -> Unit){
+        Groups.unit.each{
+            if(it != this && it is YellowUnitEntity){
+                if(it.existenceTime < existenceTime) runnable()
+            }
+        }
+    }
+    
+    private fun destroyFull(){
+        invalidateVars()
+        super.destroy()
+    }
+    
+    private fun removeFull(){
+        invalidateVars()
+        super.remove()
+    }
+    
+    fun outOfWorldBounds(): Boolean{
+        return x > Vars.world.width() * 8f || x < 0f || y > Vars.world.height() * 8f || y < 0f
+    }
+
 
     fun forceKill() {
         while(lives > 0) {
             kill()
         }
+    }
+    
+    //0 - dead/invalid + not in list, should not be in list anyways
+    //1 - alive but not in list
+    //2 - alive and in list
+    fun listed(): Int{
+        if(!isValid() || dead || (maxLives <= 0 && health  <= 0)) return 0
+        entities.each{
+            if(!dead && isValid() && it == this) return 2
+        }
+        return 1
     }
 
     override fun type(): YellowUnitType {
@@ -83,22 +129,28 @@ open class YellowUnitEntity: UnitEntity(){
     override fun kill() {
         destroy() //just call destroy(), no point in waiting
     }
+    
 
     override fun destroy() {
+        lifetimeCompare{ destroyFull() }
+        
         if(lives > 1) {
             invalidateDeath()
             return
         }
         
-        super.destroy()
+        destroyFull()
     }
 
     override fun remove() {
+        lifetimeCompare{ removeFull() }
+        
         if(!YellowPermVars.removeAllowed && lives > 1){
             return
         }
 
-        super.remove()
+
+        removeFull()
     }
 
     override fun update() {
@@ -109,6 +161,7 @@ open class YellowUnitEntity: UnitEntity(){
         }
 
         spawnedByCore = true
+        existenceTime++
 
         if(team.data().countType(type) > 1) {
             YellowPermVars.removeAllowed = true
@@ -117,7 +170,10 @@ open class YellowUnitEntity: UnitEntity(){
             YellowPermVars.removeAllowed = false
         }
         
+        if(listed() == 1) entities.add(this)
+        
         //heal surrounding units; normal units gain 10 health, player units gain either no health or a third of their current health
+        //very picky woman i will say :trollar:
         if(allowsHealing){
             Units.nearby(x, y, 15f*8f, 15f*8f){a: mindustry.gen.Unit ->
                 if(a.team == team){
@@ -153,6 +209,10 @@ open class YellowUnitEntity: UnitEntity(){
             
             franticTeleportTime--
         }
+    }
+    
+    override fun toString(){
+        return if(isValid()) "YellowUnitEntity#$id" else "(invalid) YellowUnitEntity#$id"
     }
 
 
