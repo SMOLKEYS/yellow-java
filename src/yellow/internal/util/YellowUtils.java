c0b087e76e4afd87ddfdd40ec74fbeb54ff83c21
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-11-05 15:07:33 +0800

    increase

--- src/yellow/internal/util/YellowUtils.java
+++ src/yellow/internal/util/YellowUtils.java
@@ -174,7 +174,7 @@ public class YellowUtils{
     public static void foodOpts(Table parent, Unit unit, Cons<Cell<Table>> table, Cons<Table> tableChildren){
         table(parent, c -> {
             c.width(Core.graphics.getWidth());
-            c.height(95);
+            c.height(115);
             table.get(c);
         }, cc -> {
             cc.setBackground(Styles.grayPanel);
