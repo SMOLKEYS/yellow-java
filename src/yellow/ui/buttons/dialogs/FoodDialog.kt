e922927e08db83d0ed392e129acefea4388be4cb
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-12-16 22:19:49 +0800

    thing

--- src/yellow/ui/buttons/dialogs/FoodDialog.kt
+++ src/yellow/ui/buttons/dialogs/FoodDialog.kt
@@ -27,19 +27,19 @@ open class FoodDialog : BaseDialog("Food"){
         //unit info (health + armor, sorted by lowest first (provided i can do that)), top-left
         cont.addTable(Tex.pane){
             
-        }
+        }.grow()
             
         //item selection + info (healing, healing type, etc etc), top-right
         cont.addTable(Tex.pane){
             
-        }
+        }.grow()
             
         cont.row()
             
         //unit selection (clicking a unit will make it consume the selected food item), bottom-middle
         cont.addTable(Tex.pane){
             
-        }
+        }.grow()
 
         return super.show()
     }
