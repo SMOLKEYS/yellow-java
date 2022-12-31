a4deaf3e5501b4a09165df092d637813efe3f5a7
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-12-31 15:17:44 +0800

    breaking bad

--- src/yellow/game/YellowTeam.java
+++ src/yellow/game/YellowTeam.java
@@ -4,9 +4,13 @@ import arc.graphics.*;
 import mindustry.game.*;
 
 public class YellowTeam extends Team{
-    public static final YellowTeam azmar = new YellowTeam(1000, "azmar", Color.yellow); //ill be taking id 1000
+    public static YellowTeam azmar;
     
     protected YellowTeam(int id, String name, Color color){
         super(id, name, color);
+    }
+    
+    public static void load(){
+        azmar = new YellowTeam(1000, "azmar", Color.yellow);
     }
 }
