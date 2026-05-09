15af553c8622ebf247b03da44a7023c96327c666
SMOLKEYS <smol.mindu@proton.me>
2026-05-09 12:30:17 +0800

    again

--- build.gradle.kts
+++ build.gradle.kts
@@ -418,5 +418,5 @@ project(":core"){
 }
 
 tasks.register("fullPackage"){
-    dependsOn("dex", "postJnigen", "buildFFmpegLinuxAll", "buildFFmpegWindowsAll")
+    dependsOn("dex", "postJnigen", "experimental-video-player:ffmpeg:buildFFmpegLinuxAll", "experimental-video-player:ffmpeg:buildFFmpegWindowsAll")
 }
